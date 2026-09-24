84025e746f79c03d13f02098141d3564056b625d
FayzaCH <chekaoui@gmail.com>
2026-09-24 12:02:22 +0100

    Update simulator.py
    Update simulator.py

--- client/resources/simulator.py
+++ client/resources/simulator.py
@@ -366,6 +366,7 @@ def execute(data: bytes, ip_src, cos_id):
     iperf_path='iperf' #for both container and code client
     errors = []
     def _run(cmd):
+        stdout, stderr, code = run_iperf2_cmd(cmd)
         if code !=0:
             errors.append(cmd)
         return stdout, stderr, code
